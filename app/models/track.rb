3c29c08d4574fe7890d0c74e63068a4ca11cce44
peter huyser <huyserp@gmail.com>
2021-03-27 11:52:26 +0100

    add validations, track name neednt be unique, but must be unique to a given album

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,6 +3,7 @@ class Track < ApplicationRecord
   belongs_to :album
 
   validates :number, presence: true, uniqueness: true
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates :album_id, uniqueness: { scope: :title }
   validates :length, presence: true
 end
