f6f0afaf8185c8f28730bc535760c77bdf0807ad
peter huyser <huyserp@gmail.com>
2021-03-27 11:22:15 +0100

    add validations for track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,8 @@
 class Track < ApplicationRecord
   belongs_to :side
   belongs_to :album
+
+  validates :number, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
+  validates :length, presence: true
 end
