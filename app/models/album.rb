99e89b12ae3e9996a23267f223a0ae146a825a92
peter huyser <huyserp@gmail.com>
2021-03-27 11:32:45 +0100

    add validations for album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,8 @@ class Album < ApplicationRecord
   belongs_to :user
   has_many :sides
   has_many :tracks
+
+  validates :title, presence: true
+  # must contain at lest the year relased (i.e. '.... 1964')
+  validates :release_date, presence: true, format: { with: /\A.*\d{4}.*\z/ }
 end
