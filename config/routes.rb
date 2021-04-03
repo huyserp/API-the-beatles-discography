3bf289621b0a7c8d5e26aa787aaa25fe735bdae3
peter huyser <huyserp@gmail.com>
2021-04-03 14:25:23 +0200

    add destroy route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :albums, only: [ :index, :show, :create ]
+      resources :albums, only: [ :index, :show, :create, :destroy ]
     end
   end
 
