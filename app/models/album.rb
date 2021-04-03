3c649503594cc1769e1fbe9cdc137cd72bcf77d4
peter huyser <huyserp@gmail.com>
2021-04-03 14:21:04 +0200

    update album validation, title must be unique

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,7 +3,7 @@ class Album < ApplicationRecord
   has_many :sides, dependent: :destroy
   has_many :tracks, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   # must contain at lest the year relased (i.e. '.... 1964')
   validates :release_date, presence: true, format: { with: /\A.*\d{4}.*\z/ }
 end
