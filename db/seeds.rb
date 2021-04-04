7ffecc9690b62718504293d28af002e2668b9ac3
peter huyser <huyserp@gmail.com>
2021-04-04 14:00:18 +0200

    change order of loops, creating of albums now chronological

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,56 +70,6 @@ def get_tracks_url(album)
   return tracks_url
 end
 
-magical_white.each do |album|
-  # 1. Create and save Album
-  album_record = Album.new(title: album.first, release_date: album.last)
-  album_record.user = ringo
-  album_record.save!
-
-  # 2. Scrape songs from album wikipedia page
-  album_url = get_tracks_url(album)
-  tracks_doc = Nokogiri::HTML(open(album_url.gsub(' ', '_')).read)
-  # Each class '.tracklist' is a table in the DOM and one side of the album
-  # (we only want the first 4 tables because these are the 4 sides
-  # of the white album and magical mystery tour Double EP)
-  sides = []
-  tracks_doc.search('.tracklist')[0..3].each do |element|
-    # look at each table, and split it into its rows, then map each row to only its content.
-    sides << element.search('tr').map { |child| child.text.strip }
-  end
-  sides.each_with_index do |side, index|
-    # we dont care about the headers(first element) or total time(last element)
-    side.pop
-    side.shift
-    # put the content of each side into a hash to pass to Active Record
-    side.map! do |song|
-      pattern = /^(?<number>\d+)."(?<title>.*)".*(?<length>\d+:\d+)$/
-      song_data = song.match(pattern)
-      Track.new(number: song_data[:number], title: song_data[:title], length: song_data[:length])
-    end
-
-    # 3. Create and save Sides based on index of elements in sides iteration
-    album_side = Side.new(name: "side #{index + 1}")
-    album_side.album = album_record
-    album_side.save!
-
-    # 4. Assign album and side to each track and save the track.
-    side.each do |song|
-      song.side = album_side
-      song.album = album_record
-      song.save!
-    end
-  end
-  puts "------------------------------------------------------------------"
-  puts "#{album_record.title} was created on #{album_record.release_date}."
-  puts "It was added to the database by #{album_record.user.email}."
-  puts "#{album_record.title} has #{album_record.tracks.count} songs on #{album_record.sides.count} different sides"
-  album_record.sides.each do |side|
-    puts "#{side.name} has the songs:"
-    side.tracks.each { |track| puts "#{track.number}. #{track.title}" }
-  end
-  puts "------------------------------------------------------------------"
-end
 
 album_data.each do |album|
   # 1. Create and save Album
@@ -172,4 +122,54 @@ album_data.each do |album|
   puts "------------------------------------------------------------------"
 end
 
+magical_white.each do |album|
+  # 1. Create and save Album
+  album_record = Album.new(title: album.first, release_date: album.last)
+  album_record.user = ringo
+  album_record.save!
+
+  # 2. Scrape songs from album wikipedia page
+  album_url = get_tracks_url(album)
+  tracks_doc = Nokogiri::HTML(open(album_url.gsub(' ', '_')).read)
+  # Each class '.tracklist' is a table in the DOM and one side of the album
+  # (we only want the first 4 tables because these are the 4 sides
+  # of the white album and magical mystery tour Double EP)
+  sides = []
+  tracks_doc.search('.tracklist')[0..3].each do |element|
+    # look at each table, and split it into its rows, then map each row to only its content.
+    sides << element.search('tr').map { |child| child.text.strip }
+  end
+  sides.each_with_index do |side, index|
+    # we dont care about the headers(first element) or total time(last element)
+    side.pop
+    side.shift
+    # put the content of each side into a hash to pass to Active Record
+    side.map! do |song|
+      pattern = /^(?<number>\d+)."(?<title>.*)".*(?<length>\d+:\d+)$/
+      song_data = song.match(pattern)
+      Track.new(number: song_data[:number], title: song_data[:title], length: song_data[:length])
+    end
+
+    # 3. Create and save Sides based on index of elements in sides iteration
+    album_side = Side.new(name: "side #{index + 1}")
+    album_side.album = album_record
+    album_side.save!
+
+    # 4. Assign album and side to each track and save the track.
+    side.each do |song|
+      song.side = album_side
+      song.album = album_record
+      song.save!
+    end
+  end
+  puts "------------------------------------------------------------------"
+  puts "#{album_record.title} was created on #{album_record.release_date}."
+  puts "It was added to the database by #{album_record.user.email}."
+  puts "#{album_record.title} has #{album_record.tracks.count} songs on #{album_record.sides.count} different sides"
+  album_record.sides.each do |side|
+    puts "#{side.name} has the songs:"
+    side.tracks.each { |track| puts "#{track.number}. #{track.title}" }
+  end
+  puts "------------------------------------------------------------------"
+end
 
